ccc3b149235e50e8ae91f278bc902eb9a9298523
jismartin <jismartin@gmail.com>
2024-03-18 11:23:03 +0100

    Update plots, new stationarity analysis and new EBM fit

--- notebooks/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Iterate over each batch folder in the current directory
-find "./" -mindepth 1 -maxdepth 1 -type d | while read -r folder; do
-    cd "$folder"
-    echo "Running batch in folder $folder"
-    sbath ./run_slurm.sh
-    cd ..
-done
